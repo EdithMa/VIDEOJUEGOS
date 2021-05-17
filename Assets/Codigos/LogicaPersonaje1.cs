5ef2df6ff4f4abb33f40481b4374509bb2072d7f
EdithMa <51275905+EdithMa@users.noreply.github.com>
2021-05-17 01:21:50 -0500

    Saltar

--- Assets/Codigos/LogicaPersonaje1.cs
+++ Assets/Codigos/LogicaPersonaje1.cs
@@ -8,23 +8,48 @@ public class LogicaPersonaje1 : MonoBehaviour
 	public float velocidadRotacion = 200.0f;
 	private Animator anim;
 	public float x,y;
+    public Rigidbody rb;
+    public float fuerzaSalto = 8;
+    public bool puedoSaltar;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        puedoSaltar = true;
     }
-
+    private void FixedUpdate()
+    {
+        //Estandariza el tiempo sin importar los fps
+        transform.Translate(0,0, y*Time.deltaTime * velocidadMovimiento);
+        transform.Rotate(0, x*Time.deltaTime * velocidadRotacion, 0);
+    }
     // Update is called once per frame
     void Update()
     {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
 
-        transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
-        transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
-   		
    		anim.SetFloat("VelX", x);
    		anim.SetFloat("VelY", y);
+
+           if(puedoSaltar)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                anim.SetBool("salto", true);
+                rb.AddForce(new Vector3(0, fuerzaSalto, 0), ForceMode.Impulse);
+            }
+            anim.SetBool("tocaSuelo", true);
+        }
+        else
+        {
+            estoyCayendo();
+        }
+    }
+     public void estoyCayendo()
+    {
+        anim.SetBool("tocaSuelo", false);
+        anim.SetBool("salto", false);
     }
 }
